286a20b7357b4b7470153a1af95cc82ac4ee5562
Kaya229 <taka.0229obon@gmail.com>
2025-03-11 22:24:54 -0400

    Update ui.ts

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,10 +1,10 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
-export const CHAT_HEADER: string = "Warren Buffett AI";
+export const CHAT_HEADER: string = "WarrenBuffettAI";
 export const MESSAGE_PLACEHOLDER: string = "[MESSAGE PLACEHOLDER]";
-export const FOOTER_MESSAGE: string = "Warren Buffett AI by Takachika";
+export const FOOTER_MESSAGE: string = "WallenBuffettAI by Takachika";
 export const CLEAR_BUTTON_TEXT: string = "[CLEAR BUTTON TEXT]";
 export const PAGE_TITLE: string = "[PAGE TITLE]";
-export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`Let’s study stock investment knowledge and become like Warren Buffett.;
+export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant. Let’s study stock investment knowledge and become like Warren Buffett.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
