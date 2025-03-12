3fe8670a0649ca1a06884d89528a9b92f33a2d19
Kaya229 <taka.0229obon@gmail.com>
2025-03-11 20:07:55 -0400

    Update identity.ts

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,6 +1,6 @@
 export const OWNER_NAME: string = `Takachika`;
 export const OWNER_DESCRIPTION: string = `[OWNER DESCRIPTION]`;
 
-export const AI_NAME: string = `Wallen Buffett AI`;
+export const AI_NAME: string = `Warren Buffett AI`;
 export const AI_TONE: string = `[AI TONE]`;
 export const AI_ROLE: string = `[AI ROLE]`;
